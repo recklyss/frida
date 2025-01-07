98719778ee952b0343126d25d3ed2530f255a515
recklyss <zhangjiahengpoping@gmail.com>
2025-01-07 23:46:16 +0700

    fix some lint issue

--- src/app/showcase/page.tsx
+++ src/app/showcase/page.tsx
@@ -3,6 +3,7 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { RefObject, useEffect, useId, useRef, useState } from "react";
 
+import { CloseIcon } from "@/app/_components/CloseIcon";
 import { IconArrowLeft } from "@tabler/icons-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -198,39 +199,6 @@ export default function ShowcasePage() {
     </div>
   );
 }
-
-export const CloseIcon = () => {
-  return (
-    <motion.svg
-      initial={{
-        opacity: 0,
-      }}
-      animate={{
-        opacity: 1,
-      }}
-      exit={{
-        opacity: 0,
-        transition: {
-          duration: 0.05,
-        },
-      }}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="h-4 w-4 text-black"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M18 6l-12 12" />
-      <path d="M6 6l12 12" />
-    </motion.svg>
-  );
-};
 
 const cards = [
   {
